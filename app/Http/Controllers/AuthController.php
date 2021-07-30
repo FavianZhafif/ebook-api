39d26fd757b7a875862cc4357e8894ee4d158839
zhafif.cerdas@gmail.com <zhafif.cerdas@gmail.com>
2021-07-30 20:51:43 +0700

    endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+  public function me() {
+    return ['NIS' => 3103119077,
+        'name' => 'Favian Zhafif Arkan',
+        'gender' => 'Male',
+        'phone' => '085327228234',
+        'class' => 'XII RPL 3'];
+  }
+}
